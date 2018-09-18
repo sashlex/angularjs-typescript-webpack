2f6d0cf97e7a436085a2f05cd62893393c7e05fe
mudak <sashlex@yandex.ru>
2018-09-18 22:38:19 +0300

    Update templates.

--- app/components/mc-dates/mc-dates.controller.ts
+++ app/components/mc-dates/mc-dates.controller.ts
@@ -1,5 +1,12 @@
 import { McDatesItem } from './mc-dates.model';
 
 export class McDatesController {
+   public date1: object;
+   public date2: object;
+
+   $onInit() {
+      this.date1 = new Date();
+      this.date2 = new Date();
+   }
 
 }
--- app/components/mc-dates/mc-dates.directive.ts
+++ app/components/mc-dates/mc-dates.directive.ts
@@ -1,11 +1,13 @@
 import angular from 'angular';
+import template from './mc-dates-directive.html';
 
 export class McDates implements angular.IDirective {
    restrict: string;
+   template: string;
 
    constructor() {
       this.restrict = 'A';
+      this.template = template;
    }
-   link( $scope ) {
-   }
+
 }
