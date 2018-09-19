b892c7a779c9cd6dabc02b366757f7630dfe426c
mudak <sashlex@yandex.ru>
2018-09-19 22:37:56 +0300

    Update datepicker format.

--- app/components/mc-dates/index.ts
+++ app/components/mc-dates/index.ts
@@ -2,12 +2,17 @@ import * as angular from 'angular';
 import { McDatesComponent } from './mc-dates.component';
 import { McDates } from './mc-dates.directive';
 import uiRouter from '@uirouter/angularjs';
+import moment = require( 'moment' ); // https://github.com/moment/moment/blob/develop/typing-tests/moment-tests.ts
+import 'moment/locale/ru';
+moment.locale( 'ru' );
 
 export const McDatesModule = angular
-   .module( 'mcDates', [ uiRouter ])
+   .module( 'mcDates', [
+      uiRouter,
+   ])
    .component( 'mcDatesComponent', McDatesComponent )
    .directive( 'mcDates', () => new McDates())
-   .config(( $stateProvider, $urlRouterProvider ) => {
+   .config(( $stateProvider, $urlRouterProvider, $mdDateLocaleProvider ) => {
 
       'ngInject';
       $urlRouterProvider.otherwise( '/' );
@@ -16,5 +21,12 @@ export const McDatesModule = angular
             url: '/mc-dates',
             component: 'mcDatesComponent',
          });
+
+      /* datepicker format */
+      $mdDateLocaleProvider.firstDayOfWeek = 1;
+      $mdDateLocaleProvider.shortMonths = [ 'Янв', 'Фев', 'Март', 'Апр','Май','Июнь','Июль','Авг','Сен','Окт','Ноя','Дек' ];
+      $mdDateLocaleProvider.shortDays = [ 'Вс','Пн','Вт','Ср','Чт','Пт','Сб' ];
+      $mdDateLocaleProvider.formatDate = date => ( date !== '' && date !== null ) && moment(date).format( 'DD.MM.YYYY' ) || null;
+
    })
    .name;
