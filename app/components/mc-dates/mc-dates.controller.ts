e7a6a84e795889b767af12efc9d430d7d892da13
mudak <sashlex@yandex.ru>
2018-09-20 22:25:58 +0300

    Update change date handler.

--- app/components/mc-dates/mc-dates.controller.ts
+++ app/components/mc-dates/mc-dates.controller.ts
@@ -11,12 +11,14 @@ export class McDatesController {
    }
 
    /**
-    * @param {number} num - input date number
+    * @param {number} num - input field number ( like id )
     * @return {undefined}
     */
    changeDate( num: number ) {
-      num === 1 && moment( this.mcdates.inputDate1, 'DDMMYYYY', true ).isValid() && ( this.mcdates.date1 = moment( this.mcdates.inputDate1, 'DDMMYYYY', true ).toDate());
-      num === 2 && moment( this.mcdates.inputDate2, 'DDMMYYYY', true ).isValid() && ( this.mcdates.date2 = moment( this.mcdates.inputDate2, 'DDMMYYYY', true ).toDate());
+      num === 11 && moment( this.mcdates.inputDate1, 'DDMMYYYY', true ).isValid() && ( this.mcdates.date1 = moment( this.mcdates.inputDate1, 'DDMMYYYY', true ).toDate());
+      num === 21 && moment( this.mcdates.inputDate2, 'DDMMYYYY', true ).isValid() && ( this.mcdates.date2 = moment( this.mcdates.inputDate2, 'DDMMYYYY', true ).toDate());
+      num === 12 && ( this.mcdates.inputDate1 = moment( this.mcdates.date1 ).format( 'DDMMYYYY' ));
+      num === 22 && ( this.mcdates.inputDate2 = moment( this.mcdates.date2 ).format( 'DDMMYYYY' ));
    }
 
 }
